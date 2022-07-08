2f74ea1438444c3b93ef299668f4c4820b439024
Raphael Rocha <rmoura-r@student.42.rio>
2022-07-07 21:40:43 -0300

    c s i d % OK

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 21:48:11 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/07/07 18:45:06 by rmoura-r         ###   ########.fr       */
+/*   Updated: 2022/07/07 21:34:45 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,22 +14,31 @@
 
 static int	flag_print(char a, va_list args)
 {
+	//va_list	argcpy;
+	char	*str;
+	
 	if (a == 'c')
 		ft_putchar_fd(va_arg(args, int), 1);
 	else if (a == 's')
 	{
-		ft_putstr_fd(va_arg(args, char *), 1);
-		return (ft_strlen(va_arg(args, char *)));
+		str = va_arg(args, char *);
+		ft_putstr_fd(str, 1);
+		return (ft_strlen(str));
 	}
-	//else if (a == p)  %s, %cdip, 's', 
+	//else if (a == p)
 	// p (unsigned long long?)
-	// d  // i
+	else if (a == 'i' || a == 'd')
+	{
+		str = ft_itoa(va_arg(args, int));
+		ft_putstr_fd(str, 1);
+		return (ft_strlen(str));
+	}
 	// u
 	// x
 	// X
-	// %
+	else if (a == '%')
+		ft_putchar_fd(a, 1);
 	return (1);
-
 }
 
 int	ft_printf(const char *str, ...)
@@ -46,9 +55,10 @@ int	ft_printf(const char *str, ...)
 		if (str[i] == '%')
 		{
 			len += flag_print(str[i + 1], args);
+			i++;
 		}
 		else
-			printf("%c\n", str[i]);
+			len += write(1, &str[i], 1);
 		i++;
 	}
 	va_end(args);
@@ -56,9 +66,13 @@ int	ft_printf(const char *str, ...)
 }
 
 
+#include <stdio.h>
 int	main(void)
 {
-	char	str[] = "abc";
-	ft_printf("teste %s", str);
+	char	ex[] = "abc";
+	int		i;
+	
+	i = ft_printf("%s\n%c\n%i\n%d", ex, 'k', 56, 999);
+	printf("\noutput: %i\n", i);
 }
 
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rmoura-r <rmoura-r@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 21:31:01 by rmoura-r          #+#    #+#             */
-/*   Updated: 2022/07/07 18:39:53 by rmoura-r         ###   ########.fr       */
+/*   Updated: 2022/07/07 19:22:08 by rmoura-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include "libft/libft.h"
 # include <stdarg.h>
-
-# include <stdio.h> // REMOVER !!!!!!
 
 int	ft_printf(const char *, ...);
 
